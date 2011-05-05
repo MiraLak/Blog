014938616aaf17debd2a538746d98e5ccbaf905d
MiraLak <amira.lakhal@gmail.com>
2011-05-05 23:21:01 +0200

    initialisation des repertoires rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '86191c27e0156f09717f05ff50d9666e65c631aef6b40b00ddd6637e99c6892879d989c6c77e494d0228564cf1b76622469f0d208c0f40cbce9eb68c73057daf'
